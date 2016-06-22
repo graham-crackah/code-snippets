163b176caca6c2ff5d6656bea0ab4ed45bf2a551
graham-crackah <graham@Grahams-MacBook-Pro.local>
2016-06-22 08:14:50 -0400

    trine get server
    serv?

---
+++ server.js
@@ -0,0 +1,35 @@
+function App(){
+	this.name = 'name';
+	this.version = '1';
+}
+
+App.prototype.config = function(){
+	if  (!var conf = new Config();
+}
+
+const http = require('http');
+const path = require('path');
+const spawn = require('child_process').spawn;
+
+const Config = require('./config/app');
+const Port = 
+const server = new Hapi.Server();
+const chalk = require('chalk');
+
+
+
+new Config();
+
+
+server.connection({ port : Config.port});
+const status = function (request, reply){
+  console.log('--STATUS--');
+}
+const onRequest = function (request, reply) {
+  const uri = request.raw.req.url;
+  const parsed = Url.parse(uri, false);
+  parsed.query = Qs.parse(parsed.query);
+  request.setUrl(parsed);
+  return reply.continue();
+};
+server.ext('onRequest', onRequest);
